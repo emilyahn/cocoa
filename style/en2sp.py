be0fa3398c781fdb0f02291b6b6876a927e8934a
Emily Ahn <eahn@wellesley.edu>
2018-02-06 23:53:48 -0500

    lexical codemixer and output

--- style/en2sp.py
+++ style/en2sp.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # from polyglot.text import Text
 import spacy
 from nltk.tokenize import wordpunct_tokenize
@@ -10,39 +12,66 @@ nlp_en = spacy.load('en')
 nlp_sp = spacy.load('es')
 
 def read_tables():
-	with open("data/chat_prev/bot_rule.txt") as f:
-		en_orig = f.readlines()
-	with open("data/chat_prev/sp_bot_rule.txt") as f:
-		sp_orig = f.readlines()
+	with open("data/chat_prev/en_rule.txt") as f:
+		en_orig = [line.replace('\n','') for line in f.readlines()]
+	with open("data/chat_prev/sp_rule.txt") as f:
+		sp_orig = [line.replace('\n','') for line in f.readlines()]
 	return dict([('en', en_orig), ('sp', sp_orig)])
 
+tables_dct = read_tables()
 # idx&txt params are redundant but to-be-modified later
-def sp_matrix_eng_nouns(eng_txt, tables_dct, idx):
+def sp_matrix_eng_nouns(en_txt, idx): #tables_dct, 
 	""" eng_txt: string of words (not list)
 		tables_dct: contains 2 tables, keys = en,sp
 		idx: int of which index in table is this sentence
+		[return] string of words (list separated by space)
 	"""
-	eng_doc = nlp_en(unicode(eng_txt, "utf-8"))
-	# eng_pos = [token.pos_ for token in eng_doc]
+	en_doc = nlp_en(unicode(en_txt, "utf-8"))
+	en_nouns = [en_token.text for en_token in en_doc if en_token.pos_ in ['NOUN', 'PROPN']]
+	# avoid "who"
+	en_nouns = [noun for noun in en_nouns if not noun==u"who"]
+	# print "EN NOUNS", en_nouns
 
 	sp_txt = tables_dct['sp'][idx]
 	sp_doc = nlp_sp(unicode(sp_txt, "utf-8"))
-	# sp_pos = [token.pos_ for token in sp_doc]
+	sp_nouns = []
+	for orig_i, sp_token in enumerate(sp_doc):
+		if sp_token.pos_ in ['NOUN', 'PROPN']: sp_nouns.append((sp_token.text, orig_i))
+		# capture infinitive verbs ==> turn into noun (e.g. nadar = swimming)
+		if sp_token.pos_=='VERB' and sp_token.tag_.endswith('Inf'): sp_nouns.append((sp_token.text, orig_i))
+		
+	# print "SP NOUNS", sp_nouns
 
-	for en_token in eng_doc:
-		en_pos = en_token.pos_
-		if en_pos in ['NOUN', 'PROPN']:
-			pass
+	cm_txt = [sp_token.text for sp_token in sp_doc]
+	for noun_idx, (sp_token, orig_i) in enumerate(sp_nouns):
+		if not en_nouns: break
+		if noun_idx < len(en_nouns)-1: #valid, or else is last
+			cm_txt[orig_i] = en_nouns[noun_idx]
+		else: # get last noun (probably last one anyway??)
+			cm_txt[orig_i] = en_nouns[-1]
+	return " ".join(cm_txt)
 
-sent_sp = "¿Tiene alguna ingeniería de interiores y electrónica?"
-sent_en = "Do you have any laser tag or spanish?"
+# TEST/PLAY AROUND
 
-# polyglot syntax
-# text = Text(sent)
-# words = text.words
-# tags = text.pos_tags
+# sent_sp = "Tengo dos amigos que estudiaron estudios islámicos."
+# sent_en = "I have two friends who studied islamic studies."
 
-doc = nlp_en(unicode(sent_en, "utf-8"))
+# test_dct = dict([('en', [sent_en]), ('sp', [sent_sp])])
+# print sp_matrix_eng_nouns(sent_en, test_dct, 0)
 
-for token in doc:
-    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)  
+print "STYLE 1: LEXICAL on Rule-based Bot // 6 Feb 2018"
+print "KEY:\n1. Spanish (orig)\n2. English (orig)\n3. CodeMix"
+print "*"*10
+
+for i, en_sent in enumerate(tables_dct['en']):
+	print tables_dct['sp'][i]
+	print en_sent
+	print sp_matrix_eng_nouns(en_sent, i).encode('utf-8')
+	print "*"*10
+	# if i==10: break
+
+
+
+# possible stuff in spacy's doc object:
+# for token in doc:
+#     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)  
